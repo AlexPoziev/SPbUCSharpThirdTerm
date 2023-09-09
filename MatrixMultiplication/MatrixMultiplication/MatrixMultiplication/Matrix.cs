d2d40038fd645e325994aa25a4c88ede81b563ad
Aleksey Poziev <112957370+AlexPoziev@users.noreply.github.com>
2023-09-09 16:51:21 +0300

    feat: add Matrix
    feat: add naive multiplication

---
+++ MatrixMultiplication/MatrixMultiplication/MatrixMultiplication/Matrix.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+
+namespace MatrixMultiplication.Models;
+
+public class Matrix
+{
+    public int[,] MatrixArray { get; }
+
+    public (int row, int column) Size => (MatrixArray.GetLength(0), MatrixArray.GetLength(1));
+
+    public void WriteInFile(string filePath)
+    {
+        using var writer = new StreamWriter(filePath);
+
+        for (int i = 0; i < Size.row; ++i)
+        {
+            for (int j = 0; j < Size.column; ++j)
+            {
+                writer.Write($"{MatrixArray[i, j]} ");
+            }
+
+            writer.Write(Environment.NewLine);
+        }
+    }
+
+    public Matrix(string filePath)
+    {
+        ArgumentNullException.ThrowIfNull(filePath);
+
+        string[] lines;
+
+        try
+        {
+            lines = File.ReadAllLines(filePath);
+        }
+        catch (FileNotFoundException)
+        {
+            throw;
+        }
+        
+
+        MatrixArray = new int[lines.Length, lines[0].Split(' ').Length];
+
+        for (int i = 0; i < lines.Length; ++i)
+        {
+            var line = lines[i].Split(' ').Select(int.Parse).ToArray();
+
+            if (line.Length != MatrixArray.GetLength(1))
+            {
+                throw new InvalidDataException();
+            }
+
+            for (int j = 0; i < line.Length; ++j)
+            {
+                MatrixArray[i,j] = line[j];
+            }
+        }
+    }
+
+    public Matrix(int[,] matrix)
+    {
+        MatrixArray = matrix;
+    }
+
+    public Matrix((int rowSize, int columnSize) size)
+    {
+        var random = new Random();
+
+        MatrixArray = new int[size.rowSize, size.columnSize];
+
+        for (int i = 0; i < size.rowSize; ++i)
+        {
+            for (int j = 0; j < size.columnSize; ++j)
+            {
+                MatrixArray[i, j] = random.Next(1024);
+            }
+        }
+    }
+}
+
---
+++ MatrixMultiplication/MatrixMultiplication/MatrixMultiplication/MatrixOperations.cs
@@ -0,0 +1,33 @@
+﻿using MatrixMultiplication.Models;
+
+namespace MatrixMultiplication;
+
+public static class MatrixOperations
+{
+    public static Matrix MatricesMultiplication(Matrix firstMatrix, Matrix secondMatrix)
+    {
+        ArgumentNullException.ThrowIfNull(firstMatrix);
+        ArgumentNullException.ThrowIfNull(secondMatrix);
+
+        if (firstMatrix.Size.column != secondMatrix.Size.row)
+        {
+            throw new InvalidDataException();
+        }
+
+        var matrix = new int[firstMatrix.Size.row, secondMatrix.Size.column];
+
+        for (int i = 0; i < firstMatrix.Size.row; ++i)
+        {
+            for (int j = 0; j < secondMatrix.Size.column; ++j)
+            {
+                for (int k = 0; k < firstMatrix.Size.row; ++k)
+                {
+                    matrix[i, j] += firstMatrix.MatrixArray[i, k] * secondMatrix.MatrixArray[k, j];
+                }
+            }
+        }
+
+        return new Matrix(matrix);
+    }
+}
+
